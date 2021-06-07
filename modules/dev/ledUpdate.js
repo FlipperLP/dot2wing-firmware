2f903cb291e0467ad16ec8d45a765f406685af62
Phil <philip.schaefer0@gmail.com>
2021-06-08 00:14:14 +0200

    inital init

---
+++ modules/dev/ledUpdate.js
@@ -0,0 +1,17 @@
+import ledHandler from 'rpi-ws281x-native';
+
+import rawConfig from '../../config.json';
+
+// get correct config part
+const config = rawConfig.controller.neopixel;
+
+// configure leds
+const channel = ledHandler(config.ammount, config.options);
+
+// change color
+const colorArray = channel.array;
+for (let i = 0; i < channel.count; i++) {
+  colorArray[i] = 0xffcc22;
+}
+
+ws281x.render();
