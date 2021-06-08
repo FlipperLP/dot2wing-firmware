a1ac7e4e217da85090ea96a2e36a58bba0cc0d81
Phil <philip.schaefer0@gmail.com>
2021-06-08 18:34:10 +0200

    added functions

--- modules/webSocketHandler.js
+++ modules/webSocketHandler.js
@@ -2,29 +2,35 @@ import WebSocket from 'ws';
 
 import rawConfig from '../config.json';
 
-// get correct config part
 const config = rawConfig.maweb;
 
 // get endpoint
 const endpoint = process.env.ENDPOINT || config.endpoint;
 
 // open new websocket session
-const WSconnection = new WebSocket(`ws://${endpoint}/?ma=1`);
+export const WSconnection = new WebSocket(`ws://${endpoint}/?ma=1`);
 
 // send websocket
 export function sendWebsocket(msg) { WSconnection.send(JSON.stringify(msg)); }
 
 // button press and release
-export function setButton(pressed, execIndex, buttonId) {
+export function setButton(session, pressed, execIndex, buttonId) {
   sendWebsocket({
-    requestType: 'playbacks_userInput', execIndex: execIndex - 1, pageIndex: 0, buttonId: buttonId || 0, pressed, released: !pressed, type: 0, session: config.maweb.activeSession,
+    requestType: 'playbacks_userInput',
+    session,
+    execIndex: execIndex - 1,
+    pageIndex: 0,
+    buttonId: buttonId || 0,
+    pressed,
+    released: !pressed,
+    type: 0,
   });
 }
 
 // set fader value
-export function setFader(faderValue, execIndex) {
+export function setFader(session, faderValue, execIndex) {
   sendWebsocket({
-    requestType: 'playbacks_userInput', execIndex: execIndex - 1, pageIndex: 0, faderValue, type: 1, session: config.maweb.activeSession,
+    requestType: 'playbacks_userInput', execIndex: execIndex - 1, pageIndex: 0, faderValue, type: 1, session,
   });
 }
 
