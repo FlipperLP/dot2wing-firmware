f34960f493f1eef377feeec090d5364481c64e30
Phil <philip.schaefer0@gmail.com>
2021-06-08 23:18:58 +0200

    trail

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-/* eslint-disable no-bitwise */
 // eslint-disable-next-line import/no-unresolved
 import ledHandler from 'rpi-ws281x';
 
 // One time initialization
 ledHandler.configure({
-  leds: 3, gpio: 18, brightness: 100, dma: 10, stripType: 'grb',
+  leds: 15, gpio: 18, brightness: 100, dma: 10, stripType: 'grb',
 });
 
 // Create my pixels
 const pixels = new Uint32Array(3);
 
-pixels[0] = (255 << 16) | (0 << 8) | 0;
-pixels[1] = (0 << 16) | (255 << 8) | 0;
-pixels[2] = (0 << 16) | (0 << 8) | 255;
+const red = 255;
+const green = 0;
+const blue = 0;
+// eslint-disable-next-line no-bitwise
+const color = (red << 16) | (green << 8) | blue;
+// eslint-disable-next-line no-bitwise
+const colorBlack = (green << 16) | (green << 8) | blue;
+
+while (true) {
+  pixels.forEach((plx, i) => {
+    setInterval(() => {
+      pixels[i - 1] = colorBlack;
+      pixels[i] = color;
+      ledHandler.render(pixels);
+    }, 500);
+  });
+}
 
 // Render pixels to the Neopixel strip
-ledHandler.render(pixels);
