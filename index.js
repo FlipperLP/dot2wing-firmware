f69db8fb0d176c9a74b6de017df31687d5fdf558
Phil <philip.schaefer0@gmail.com>
2021-06-08 19:51:15 +0200

    added debug functioniality

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ import config from './config.json';
 import { prettify } from './modules/prettify';
 
 import {
-  sendWebsocket, setButton, setFader, getSetSession,
+  sendWebsocket, setButton, setFader, getSetSession, WSconnection,
 } from './modules/webSocketHandler';
 
 // define gpio pin
-const gpio4 = gpio.export(4, { direction: gpio.DIRECTION.IN });
+let gpio4;
+if (!process.env.debug) gpio4 = gpio.export(4, { direction: gpio.DIRECTION.IN });
 
 // keep session alive
 function keepAlive() {
   setInterval(() => {
     // call data to get playback info
     getSetSession(config.maweb.activeSession);
-    console.log('Keepalive', config.maweb.activeSession);
-  }, 10000);
-  // }, config.maweb.keepAlive);
+    if (!gpio4) console.log('Keepalive Session', config.maweb.activeSession);
+  }, config.maweb.keepAlive);
 }
 
 // call data and post data from input
@@ -37,6 +37,16 @@ function mainLoop() {
 function callbackData(rawData) {
   const cleanData = prettify(rawData);
   console.log(cleanData[5].button1.isRun, cleanData[5].button2.isRun, cleanData[5].faderExecutor.isRun);
+}
+
+function debugLoop() {
+  let val = false;
+  setInterval(() => {
+    // call data to get playback info
+    setButton(val, 101, 0);
+    console.debug('set button to', val);
+    val = !val;
+  }, 1000);
 }
 
 // login provided session
@@ -58,12 +68,13 @@ function loginSession(requestType, argument) {
       // check if mainloop already runns; no double loop
       if (!config.maweb.mainLoopRunning) config.maweb.mainLoopRunning = true;
       else return;
-      // call mainloop
+      // call loops
       keepAlive();
       mainLoop();
+      if (!gpio4) debugLoop();
       break;
     default:
-      getsetSession();
+      getSetSession();
       break;
   }
 }
@@ -71,30 +82,30 @@ function loginSession(requestType, argument) {
 // login websocket
 WSconnection.onopen = () => loginSession();
 
-gpio4.on('change', (val) => {
-  console.debug(val);
-  switch (val) {
-    case 1:
-      setButton(true, 106, 0);
-      return;
-    case 0:
-      setButton(false, 106, 0);
-      return;
-    default:
-      return;
-  }
-});
+if (gpio4) {
+  gpio4.on('change', (val) => {
+    switch (val) {
+      case 1:
+        setButton(true, 106, 0);
+        return;
+      case 0:
+        setButton(false, 106, 0);
+        return;
+      default:
+        return;
+    }
+  });
+}
 
 // websocket awnser splitter
 WSconnection.onmessage = (msg) => {
   const response = JSON.parse(msg.data);
-  // console.debug(response);
+  // if (!gpio4) console.debug(response);
   if (response.status === 'server ready') return;
   if (response.forceLogin) return loginSession('login', response.session);
   if (response.result) return loginSession('afterLogin', response.result);
   if (response.responseType === 'playbacks') return callbackData(response);
   // FIXME: to awoid having too many sessions, check how to reuse old one in maweb
-  // return loginSession();
 };
 
 // post error
