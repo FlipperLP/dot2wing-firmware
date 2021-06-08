e4ca7dfdba32e9541206491d7f7b0c0d10cc1ae4
Phil <philip.schaefer0@gmail.com>
2021-06-08 22:26:26 +0200

    added color code

--- index.js
+++ index.js
@@ -2,10 +2,22 @@
 import ledHandler from 'rpi-ws281x';
 
 // One time initialization
-ledHandler.configure({ leds: 3 });
+ledHandler.configure({
+  leds: 3, gpio: 17, brightness: 10, dma: 10, stripType: 'grb',
+});
 
 // Create my pixels
 const pixels = new Uint32Array(3);
+
+const red = 255;
+const green = 0;
+const blue = 0;
+// eslint-disable-next-line no-bitwise
+const color = (red << 16) | (green << 8) | blue;
+
+pixels.forEach((plx, i) => {
+  pixels[i] = color;
+});
 
 // Render pixels to the Neopixel strip
 ws281x.render(pixels);
