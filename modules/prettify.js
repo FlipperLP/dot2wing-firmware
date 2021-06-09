9837ee210d48f537d0555cbaab6eeec20cd8d92a
Phil <philip.schaefer0@gmail.com>
2021-06-09 23:33:37 +0200

    revised

--- modules/prettify.js
+++ modules/prettify.js
@@ -1,38 +1,64 @@
-export function prettify(rawData) {
-  // row
-  const rows = [];
-  for (let row = 0; row <= 8; row++) {
-    // buttonrow
-    // TODO:
-    // color: rawData.itemGroups[0].items[0][0].bdC
-    // empty: rawData.itemGroups[0].items[0][0].i.c = '#C0C0C0'
-    const button1 = {
-      name: rawData.itemGroups[1].items[row][0].tt.t,
-      id: rawData.itemGroups[1].items[0][0].iExec,
-      isRun: rawData.itemGroups[1].items[row][0].isRun,
-    };
-    const button2 = {
-      name: rawData.itemGroups[2].items[row][0].tt.t,
-      id: rawData.itemGroups[2].items[0][0].iExec,
-      isRun: rawData.itemGroups[2].items[row][0].isRun,
-    };
-    // faderExecutor
-    const name = rawData.itemGroups[0].items[5][0].tt.t;
-    const isRun = rawData.itemGroups[0].items[5][0].isRun;
-    const go = {
-      name: rawData.itemGroups[0].items[row][0].executorBlocks[0].button1.t,
-    };
-    const flash = {
-      name: rawData.itemGroups[0].items[row][0].executorBlocks[0].button2.t,
-    };
-    const faderExecutor = {
-      name, isRun, go, flash,
-    };
-    rows.push({
-      row, button1, button2, faderExecutor,
-    });
-  }
-  return rows;
+function parseButton(rawButton) {
+  const button = {};
+  button.name = rawButton.tt.t;
+  button.isRun = rawButton.isRun === 1;
+  button.empty = rawButton.i.c === '#000000';
+  button.color = rawButton.bdC;
+  return button;
 }
 
-export { prettify as default };
+function buttonRow(row) {
+  const buttons = [];
+  row.items.forEach((buttonRaw) => {
+    buttons.push(parseButton(buttonRaw[0]));
+  });
+  return buttons;
+}
+
+// function parseFaderButton(rawButton) {
+//   console.log(rawButton);
+// }
+
+function parseFader(rawFader) {
+  const output = {};
+  output.fader = {};
+  const execBlocks = rawFader.executorBlocks[0];
+  output.fader.name = rawFader.tt.t;
+  output.fader.isRun = rawFader.isRun === 1;
+  output.fader.empty = rawFader.i.c === '#000000';
+  output.fader.color = rawFader.bdC;
+  output.fader.value = execBlocks.fader.v;
+  // output.upperButton = { isPressed: parseFaderButton(execBlocks.button1) };
+  // output.lowerButton = { isPressed: parseFaderButton(execBlocks.button2) };
+  return output;
+}
+
+function faderRow(row) {
+  const faders = [];
+  row.items.forEach((faderRaw) => {
+    faders.push(parseFader(faderRaw[0]));
+  });
+  return faders;
+}
+
+export function parseValues(rawData) {
+  const output = [];
+  rawData.itemGroups.forEach((row) => {
+    let parsedFaders;
+    let parsedButtons;
+    switch (row.itemsType) {
+      case 2:
+        parsedFaders = faderRow(row);
+        break;
+      case 3:
+        parsedButtons = buttonRow(row);
+        break;
+      default: break;
+    }
+    if (parsedFaders) output.fader = parsedFaders;
+    if (parsedButtons) output.push(parsedButtons);
+  });
+  return output;
+}
+
+export { parseValues as default };
