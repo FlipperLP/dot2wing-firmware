ce712d119c3bc69cf83afc8d0f663b995e3c4bf6
Phil <philip.schaefer0@gmail.com>
2021-06-09 23:33:19 +0200

    moved functions to own file

--- index.js
+++ index.js
@@ -1,118 +1,36 @@
-import md5 from 'md5';
-
-import gpio from 'gpio';
+import rpio from 'rpio';
 
 import config from './config.json';
 
-import { prettify } from './modules/prettify';
+import { WSconnection } from './modules/webSocket';
 
 import {
-  sendWebsocket, setButton, setFader, getSetSession, WSconnection,
-} from './modules/webSocketHandler';
+  setButton, setFader, loginSession, websocketAnswer,
+} from './modules/maRemote';
 
 // define gpio pin
-let gpio4;
-if (!process.env.debug) gpio4 = gpio.export(4, { direction: gpio.DIRECTION.IN });
-
-// keep session alive
-function heartbeat() {
-  setInterval(() => {
-    // call data to get playback info
-    getSetSession(config.maweb.activeSession);
-    if (!gpio4) console.log('Heartbeat sent! Session', config.maweb.activeSession);
-  }, config.maweb.keepAlive);
-}
-
-// call data and post data from input
-function mainLoop() {
-  setInterval(() => {
-    // call data to get playback info
-    sendWebsocket({
-      requestType: 'playbacks', startIndex: [0, 100, 200], itemsCount: [14, 14, 14], pageIndex: 0, itemsType: [2, 3, 3], view: 2, execButtonViewMode: 1, buttonsViewMode: 0, session: config.maweb.activeSession, maxRequests: 1,
-    });
-  }, 30);
-}
-
-// get Data from playback
-function callbackData(rawData) {
-  const cleanData = prettify(rawData);
-  console.log(cleanData[5].button1.isRun, cleanData[5].button2.isRun, cleanData[5].faderExecutor.isRun);
-}
-
-function debugLoop() {
-  let val = false;
-  setInterval(() => {
-    // get a random button
-    const randomChoice = `${Math.floor(Math.random() * 2) + 1}0${Math.floor(Math.random() * 6) + 1}`;
-    // call data to get playback info
-    setButton(config.maweb.activeSession, val, randomChoice, 0);
-    console.debug('set button', randomChoice, 'to', val);
-    val = !val;
-  }, 100);
-}
-
-// login provided session
-function loginSession(requestType, argument) {
-  switch (requestType) {
-    case 'login':
-      config.maweb.activeSession = argument;
-      const creds = config.maweb.creds;
-      const username = creds.username;
-      const password = md5(process.env.PASSWORD || creds.password);
-      sendWebsocket({
-        requestType, username, password, session: argument, maxRequests: 10,
-      });
-      break;
-    case 'afterLogin':
-      // post active session
-      console.debug('Login:', argument);
-      console.debug('ActiveSession:', config.maweb.activeSession);
-      // check if mainloop already runns; no double loop
-      if (!config.maweb.mainLoopRunning) config.maweb.mainLoopRunning = true;
-      else return;
-      // call loops
-      heartbeat();
-      mainLoop();
-      if (!gpio4) debugLoop();
-      break;
-    default:
-      getSetSession();
-      break;
-  }
+if (!process.env.debug) {
+  config.controller.gpio.buttons.rows.forEach((row) => {
+    rpio.open(row, rpio.OUTPUT, rpio.PULL_UP);
+  });
 }
 
 // login websocket
 WSconnection.onopen = () => loginSession();
 
-if (gpio4) {
-  gpio4.on('change', (val) => {
-    switch (val) {
-      case 1:
-        setButton(true, 106, 0);
-        return;
-      case 0:
-        setButton(false, 106, 0);
-        return;
-      default:
-        return;
-    }
-  });
-}
+// setButton(true, 106, 0);
 
-// websocket awnser splitter
-WSconnection.onmessage = (msg) => {
-  const response = JSON.parse(msg.data);
-  // if (!gpio4) console.debug(response);
-  if (response.status === 'server ready') return;
-  if (response.forceLogin) return loginSession('login', response.session);
-  if (response.result) return loginSession('afterLogin', response.result);
-  if (response.responseType === 'playbacks') return callbackData(response);
-  // FIXME: to awoid having too many sessions, check how to reuse old one in maweb
-  // TODO: check if dot2 died
-};
+// while (true) {
+//   config.controller.GPIORows.forEach((row) => {
+//     for (let i = 0; i < 8; i++) {
+//       const element = array[i];
+//     }
+//   });
+// }
 
-// post error
+WSconnection.onmessage = (msg) => websocketAnswer(msg);
 WSconnection.onerror = (error) => console.log(`WebSocket error: ${error}`);
-
-WSconnection.onclose = () => console.log('disconnected');
-// TODO: kill process
+WSconnection.onclose = () => {
+  console.error('Disconnected! Exiting...');
+  process.exit(1);
+};
---
+++ modules/maRemote.js
@@ -0,0 +1,138 @@
+import md5 from 'md5';
+
+import { sendWebsocket } from './webSocket';
+
+import { parseValues } from './prettify';
+
+import config from '../config.json';
+
+let session;
+
+// button press and release
+export function setButton(pressed, execIndex, buttonId) {
+  sendWebsocket({
+    requestType: 'playbacks_userInput',
+    session,
+    execIndex: execIndex - 1,
+    pageIndex: 0,
+    buttonId: buttonId || 0,
+    pressed,
+    released: !pressed,
+    type: 0,
+  });
+}
+
+// set fader value
+export function setFader(rawFaderValue, execIndex) {
+  const faderValue = rawFaderValue / 255;
+  sendWebsocket({
+    requestType: 'playbacks_userInput',
+    session,
+    execIndex: execIndex - 1,
+    pageIndex: 0,
+    faderValue,
+    type: 1,
+  });
+}
+
+// set session ID
+export function setSession(inputSession) {
+  // IMPORTANT update session for functions
+  session = inputSession;
+  // send session
+  sendWebsocket({ session: session || 0 });
+}
+
+// call data to get playback info
+export function getPlayback() {
+  sendWebsocket({
+    requestType: 'playbacks',
+    session,
+    startIndex: [0, 100, 200],
+    itemsCount: [8, 8, 8],
+    pageIndex: 0,
+    itemsType: [2, 3, 3],
+  });
+}
+
+// keep session alive
+function heartbeatLoop() {
+  setInterval(() => {
+    // call data to get playback info
+    setSession(config.maweb.activeSession);
+    if (process.env.debug) console.log('Heartbeat sent! Session', config.maweb.activeSession);
+  }, config.maweb.keepAlive);
+}
+
+// call data and post data from input
+function mainLoop() {
+  setInterval(() => {
+    getPlayback();
+  }, 100);
+}
+
+function debugLoop() {
+  let val = false;
+  setInterval(() => {
+    // get a random button
+    const randomChoice = `${Math.floor(Math.random() * 2) + 1}0${Math.floor(Math.random() * 6) + 1}`;
+    // call data to get playback info
+    setButton(val, randomChoice, 0);
+    // setButton(val, 101, 0);
+    setFader(Math.floor(Math.random() * 256), Math.floor(Math.random() * 6) + 1);
+    // console.debug('set button', randomChoice, 'to', val);
+    val = !val;
+  }, 30);
+}
+
+// get Data from playback
+function playbackData(rawData) {
+  const cleanData = parseValues(rawData);
+  // console.log(cleanData[0][0].name, cleanData[0][0].isRun);
+  // console.log(cleanData[1][0].name, cleanData[1][0].isRun);
+  // console.log(cleanData.fader[0].fader.name, cleanData.fader[0].fader.value);
+}
+
+// login provided session
+export function loginSession(requestType, argument) {
+  switch (requestType) {
+    case 'login':
+      config.maweb.activeSession = argument;
+      const creds = config.maweb.creds;
+      const username = creds.username;
+      const password = md5(process.env.PASSWORD || creds.password);
+      sendWebsocket({
+        requestType, username, password, session: argument, maxRequests: 10,
+      });
+      break;
+    case 'afterLogin':
+      // post active session
+      console.debug('Login:', argument);
+      console.debug('ActiveSession:', config.maweb.activeSession);
+      // check if mainloop already runns; no double loop
+      if (!config.maweb.mainLoopRunning) config.maweb.mainLoopRunning = true;
+      else return;
+      // inform functions
+      setSession(config.maweb.activeSession);
+      // call loops
+      heartbeatLoop();
+      mainLoop();
+      if (process.env.debug) debugLoop();
+      break;
+    default:
+      setSession();
+      break;
+  }
+}
+
+// answer from websocket
+export function websocketAnswer(msg) {
+  const response = JSON.parse(msg.data);
+  // if (process.env.debug) console.debug(response);
+  if (response.status === 'server ready') return;
+  if (response.forceLogin) return loginSession('login', response.session);
+  if (response.result) return loginSession('afterLogin', response.result);
+  if (response.responseType === 'playbacks') return playbackData(response);
+  // FIXME: to awoid having too many sessions, check how to reuse old one in maweb
+  // TODO: check if dot2 died
+}
