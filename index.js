4690ca89944d62fd0b785a3612bc6660adc06cd4
Phil <philip.schaefer0@gmail.com>
2021-06-11 21:04:45 +0200

    changed ammount

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import rpio from 'rpio';
 
 rpio.open(16, rpio.OUTPUT, rpio.LOW);
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 6000; i++) {
   /* On for 1 second */
   rpio.write(16, rpio.HIGH);
   rpio.msleep(10);
