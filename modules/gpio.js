6674b86122013fcd9147b37f08658c8634a52ceb
Phil <philip.schaefer0@gmail.com>
2021-06-11 20:49:21 +0200

    slower for debugung

--- modules/gpio.js
+++ modules/gpio.js
@@ -52,7 +52,7 @@ function checkNewButton() {
     for (let collum = 0; collum <= 7; collum++) {
       const binary = dec2bin(collum);
       output.forEach((pin, i) => rpio.write(pin, setBin(binary[i])));
-      rpio.msleep(1);
+      rpio.msleep(10);
       // read value
       input.forEach((pin, row) => {
         const newVal = readPin(pin);
@@ -63,7 +63,7 @@ function checkNewButton() {
         }
       });
     }
-  }, 10);
+  }, 100);
 }
 
 export function initGPIO() {
