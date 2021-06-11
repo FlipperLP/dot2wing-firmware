385a7f6ff42b76d99497dc25a5990237c09e1f1a
Phil <philip.schaefer0@gmail.com>
2021-06-11 21:03:45 +0200

    changed ms

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ rpio.open(16, rpio.OUTPUT, rpio.LOW);
 for (let i = 0; i < 5; i++) {
   /* On for 1 second */
   rpio.write(16, rpio.HIGH);
-  rpio.sleep(1);
+  rpio.msleep(10);
 
   /* Off for half a second (500ms) */
   rpio.write(16, rpio.LOW);
-  rpio.msleep(500);
+  rpio.msleep(10);
 }
