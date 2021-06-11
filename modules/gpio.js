288e40086c74eb73fda45fcf7dc289c44124a4ae
Phil <philip.schaefer0@gmail.com>
2021-06-11 21:11:43 +0200

    forgotten kids

--- modules/gpio.js
+++ modules/gpio.js
@@ -68,14 +68,14 @@ function checkNewButton() {
 }
 
 export function initGPIO() {
-  // if (!process.env.debug) rpio.init({ mock: config.controller.gpio.mock });
-  // // define gpio pins
-  // input.forEach((row) => {
-  //   rpio.open(row, rpio.INPUT, rpio.PULL_UP);
-  // });
-  // output.forEach((row) => {
-  //   rpio.open(row, rpio.OUTPUT);
-  // });
+  if (!process.env.debug) rpio.init({ mock: config.controller.gpio.mock });
+  // define gpio pins
+  input.forEach((row) => {
+    rpio.open(row, rpio.INPUT, rpio.PULL_UP);
+  });
+  output.forEach((row) => {
+    rpio.open(row, rpio.OUTPUT);
+  });
   // start loop
   checkNewButton();
 }
