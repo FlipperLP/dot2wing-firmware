fbdff9948660d3437d6191660f7de3cf80494c7f
Phil <philip.schaefer0@gmail.com>
2021-06-11 23:56:52 +0200

    safsdf

--- index.js
+++ index.js
@@ -39,11 +39,9 @@ function checkNewButton() {
   setInterval(() => {
     for (let collum = 0; collum <= 7; collum++) {
       const binary = dec2bin(collum);
-      // output.forEach((pin, i) => );
-      console.log(Number(binary[2]) || 0);
       rpio.write(16, Number(binary[0]));
-      rpio.write(18, Number(binary[1]));
-      rpio.write(22, Number(binary[1]));
+      rpio.write(18, Number(binary[1]) || 0);
+      rpio.write(22, Number(binary[2]) || 0);
       rpio.msleep(10);
       // read value
       input.forEach((pin, row) => {
