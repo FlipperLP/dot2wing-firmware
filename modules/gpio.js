0501ec8ae09d3e4e9961eed19415c1fb0eaee6ae
Phil <philip.schaefer0@gmail.com>
2021-06-11 21:54:21 +0200

    dgdg

--- modules/gpio.js
+++ modules/gpio.js
@@ -10,7 +10,7 @@ const input = config.controller.gpio.buttons.input;
 const output = config.controller.gpio.buttons.output;
 
 function dec2bin(dec) {
-  return Number(dec).toString(2).split('');
+  return Number(dec).toString(2).split('').reverse();
 }
 
 function sendButton(value, buttonIndex, buttonRow) {
@@ -42,12 +42,12 @@ function checkNewButton() {
   prevAvlues.push(vals2);
   setInterval(() => {
     for (let collum = 0; collum <= 7; collum++) {
-      const binary = dec2bin(collum).reverse();
+      const binary = dec2bin(collum);
       // output.forEach((pin, i) => );
       console.log(Number(binary[2]) || 0);
-      rpio.write(16, Number(binary[0]) || 0);
-      rpio.write(18, Number(binary[1]) || 0);
-      rpio.write(22, Number(binary[2]) || 0);
+      rpio.write(16, 0);
+      rpio.write(18, 0);
+      rpio.write(22, 0);
       rpio.msleep(10);
       // read value
       input.forEach((pin, row) => {
