9b87dbb66f299539362f8b0e2269d5ad49ddaf53
Phil <philip.schaefer0@gmail.com>
2021-06-11 23:45:07 +0200

    asdf

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ function test() {
     for (let collum = 0; collum <= 7; collum++) {
       const binary = dec2bin(collum);
       rpio.write(16, Number(binary[0]));
+      rpio.write(18, Number(binary[0]));
+      rpio.write(22, Number(binary[0]));
       rpio.msleep(10);
     }
   }, 100);
