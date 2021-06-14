d378c1a2c92eb79d3ed0df71b84955278f354b31
Phil <philip.schaefer0@gmail.com>
2021-06-15 00:30:38 +0200

    asfsaf

--- modules/gpio.js
+++ modules/gpio.js
@@ -82,5 +82,6 @@ export function initOLED() {
 }
 
 export function setOLED(data) {
+  oled.clearDisplay();
   oled.writeString(1, 1, font, `    ${Math.ceil(data.fader[0].fader.value * 100)}%`, 'WHITE');
 }
