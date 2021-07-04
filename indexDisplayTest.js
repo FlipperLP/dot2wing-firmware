98b01a59278e156bcb642c492f445c19a889c396
Andre <a.michelberger@web.de>
2021-07-04 14:40:33 +0200

    preproduction

---
+++ indexDisplayTest.js
@@ -0,0 +1,19 @@
+import rpio from 'rpio';
+
+import Oled from 'sh1106-js';
+
+import font from 'oled-font-5x7';
+
+// Rpio
+rpio.init({
+  gpiomem: false,
+  mapping: 'physical',
+});
+
+
+// Oled
+const oled = new Oled({rpio});
+
+oled.clearDisplay();
+oled.drawRect(0, 0, 128, 64, 'WHITE');
+oled.writeString(64, 30, font, 'METHS', 'WHITE');
