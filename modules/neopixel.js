b205b00b1c2c732da45b4ea1a6c8ec41923c60a3
Andre <a.michelberger@web.de>
2021-07-18 16:05:14 +0200

    added fade multipier

--- modules/neopixel.js
+++ modules/neopixel.js
@@ -34,7 +34,7 @@ export function setPixels(data) {
         if (button.empty) setColor = 0;
       } else {
         // eslint-disable-next-line no-bitwise
-        if (button.fader.isRun) setColor = (color.r << 16) | (color.g << 8) | color.b;
+        if (button.fader.isRun) setColor = (color.r * button.fader.value << 16) | (color.g * button.fader.value << 8) | color.b * button.fader.value;
         // TODO: Better bitwise handler
         if (button.fader.empty) setColor = 0;
       }
