0d068331dfaf016e116f97d486b338694a3f0754
Andre <a.michelberger@web.de>
2021-07-18 13:22:49 +0200

    added all rows

--- modules/neopixel.js
+++ modules/neopixel.js
@@ -16,21 +16,23 @@ function hexToRgb(hex) {
 }
 
 export function setPixels(data) {
-  data[0].forEach((button, i) => {
-    let color = {};
-    if (allConfig.maweb.appType === 'dot2') {
-      color.r = config.dot2Color.red;
-      color.g = config.dot2Color.green;
-      color.b = config.dot2Color.blue;
-    } else color = hexToRgb(button.color);
-    const multipier = config.isRunMultipier;
-    // eslint-disable-next-line no-bitwise
-    let setColor = (color.r * multipier << 16) | (color.g * multipier << 8) | color.b * multipier;
-    // eslint-disable-next-line no-bitwise
-    if (button.isRun) setColor = (color.r << 16) | (color.g << 8) | color.b;
-    // TODO: Better bitwise handler
-    if (button.empty) setColor = 0;
-    pixels[i] = setColor;
+  data.forEach((row) => {
+    row.forEach((button, i) => {
+      let color = {};
+      if (allConfig.maweb.appType === 'dot2') {
+        color.r = config.dot2Color.red;
+        color.g = config.dot2Color.green;
+        color.b = config.dot2Color.blue;
+      } else color = hexToRgb(button.color);
+      const multipier = config.isRunMultipier;
+      // eslint-disable-next-line no-bitwise
+      let setColor = (color.r * multipier << 16) | (color.g * multipier << 8) | color.b * multipier;
+      // eslint-disable-next-line no-bitwise
+      if (button.isRun) setColor = (color.r << 16) | (color.g << 8) | color.b;
+      // TODO: Better bitwise handler
+      if (button.empty) setColor = 0;
+      pixels[i] = setColor;
+    });
   });
 }
 
