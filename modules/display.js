dcd445c5da803399df730cb819cf532c6eb571f1
Andre <a.michelberger@web.de>
2021-07-18 19:29:37 +0200

    sdfdsf

--- modules/display.js
+++ modules/display.js
@@ -4,8 +4,6 @@ import Oled from 'sh1106-js';
 
 import font from 'oled-font-5x7';
 
-import config from '../config.json';
-
 // eslint-disable-next-line no-buffer-constructor
 // const setChannel1 = new Buffer([0x01]);
 // eslint-disable-next-line no-buffer-constructor
@@ -15,19 +13,10 @@ const setChannel2 = new Buffer([0x02]);
 // eslint-disable-next-line no-buffer-constructor
 const setChannelAll = new Buffer([0xFF]);
 
-const outputData = config.controller.gpio.displays.outputData;
-
-const outputMultiplexer = config.controller.gpio.displays.outputMultiplexer;
-
 let oled;
 
-function setMultiplexer(params) {
-
-}
 
 export function initOLED() {
-  rpio.i2cSetBaudRate(400000);
-
   rpio.i2cSetSlaveAddress(0x70);
   rpio.i2cWrite(setChannelAll);
 
@@ -36,7 +25,6 @@ export function initOLED() {
   // rotate display
   [0xA1, 0xC8].forEach((cmd) => rpio.i2cWrite(Buffer.from([0x00, cmd])));
   // set lower baudrate
-  // rpio.i2cSetBaudRate(100000);
   rpio.i2cSetBaudRate(400000);
   // enable display
   oled.turnOnDisplay();
@@ -62,11 +50,6 @@ export function initOLED() {
     rpio.i2cSetSlaveAddress(0x3c);
     oled.writeString(64, 30, font, displayValue2 + '%  ', 'WHITE', false);
     oled.update();
-
-    // rpio.i2cSetSlaveAddress(0x70);
-    // rpio.i2cWrite(setChannelAll);
-    // rpio.i2cSetSlaveAddress(0x3c);
-    // oled.update();
   }, 100);
 }
 
@@ -76,10 +59,5 @@ let displayValue2 = 0;
 export function setOLED(data) {
   displayValue1 = Math.ceil(data[0][0].fader.value * 100);
   displayValue2 = Math.ceil(data[0][1].fader.value * 100);
-  console.log(displayValue1, ' ', displayValue2);
-
-  // rpio.i2cSetSlaveAddress(0x70);
-  // rpio.i2cWrite(setChannel1);
-  // rpio.i2cSetSlaveAddress(0x3c);
-  // oled.writeString(64, 30, font, `${Math.ceil(data[0][1].fader.value * 100)}%  `, 'WHITE', false);
+  // console.log(displayValue1, ' ', displayValue2);
 }
