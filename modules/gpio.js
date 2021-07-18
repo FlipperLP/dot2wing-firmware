87cedff33740c7828a810c1cc86cad5bf287f034
Andre <a.michelberger@web.de>
2021-07-18 16:50:08 +0200

    math changed

--- modules/gpio.js
+++ modules/gpio.js
@@ -90,8 +90,8 @@ function checkNewButton() {
         rpio.msleep(config.controller.gpio.fader.waitTilRead);
         // read out ADC
         rpio.i2cRead(ADCRead, 2);
-        // newFaderVal = 0 * newFaderVal + 1 * (ADCRead.readInt8(0) * 256 + ADCRead.readInt8(1));
-        newFaderVal = (ADCRead.readInt8(0) * 256 + ADCRead.readInt8(1));
+        newFaderVal = 0.5 * newFaderVal + 0.5 * (ADCRead.readInt8(0) * 256 + ADCRead.readInt8(1));
+        // newFaderVal = (ADCRead.readInt8(0) * 256 + ADCRead.readInt8(1));
       }
 
       // // start ADC sampling
