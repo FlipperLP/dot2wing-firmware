85307a9e300b65a00e3b06c73b9debb22013986f
Andre <a.michelberger@web.de>
2021-07-18 18:11:49 +0200

    display update change

--- modules/display.js
+++ modules/display.js
@@ -50,11 +50,15 @@ export function setOLED(data) {
   rpio.i2cWrite(setChannel1);
 
   rpio.i2cSetSlaveAddress(0x3c);
-  oled.writeString(64, 30, font, `${Math.ceil(data[0][0].fader.value * 100)}%  `, 'WHITE');
+  oled.writeString(64, 30, font, `${Math.ceil(data[0][0].fader.value * 100)}%  `, 'WHITE', false);
+
+  oled.update();
 
   rpio.i2cSetSlaveAddress(0x70);
   rpio.i2cWrite(setChannel2);
 
   rpio.i2cSetSlaveAddress(0x3c);
-  oled.writeString(64, 30, font, `${Math.ceil(data[0][1].fader.value * 100)}%  `, 'WHITE');
+  oled.writeString(64, 30, font, `${Math.ceil(data[0][1].fader.value * 100)}%  `, 'WHITE', false);
+
+  oled.update();
 }
