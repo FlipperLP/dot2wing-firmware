f4650780ba3bcfa5f70a0bf627210acb371e3062
Andre <a.michelberger@web.de>
2021-07-18 19:35:57 +0200

    ferfref

--- modules/display.js
+++ modules/display.js
@@ -23,7 +23,6 @@ export function initOLED() {
   rpio.i2cSetSlaveAddress(0x3c);
   oled = new Oled({ rpio, address: 0x3c});
   // rotate display
-  rpio.i2cSetSlaveAddress(0x3c);
   [0xA1, 0xC8].forEach((cmd) => rpio.i2cWrite(Buffer.from([0x00, cmd])));
   // set lower baudrate
   rpio.i2cSetBaudRate(400000);
@@ -43,14 +42,14 @@ export function initOLED() {
     oled.writeString(64, 30, font, displayValue1 + '%  ', 'WHITE', false);
     oled.update();
 
-    // rpio.msleep(50);
+    rpio.msleep(50);
 
-    // rpio.i2cSetSlaveAddress(0x70);
-    // rpio.i2cWrite(setChannel2);
+    rpio.i2cSetSlaveAddress(0x70);
+    rpio.i2cWrite(setChannel2);
 
-    // rpio.i2cSetSlaveAddress(0x3c);
-    // oled.writeString(64, 30, font, displayValue2 + '%  ', 'WHITE', false);
-    // oled.update();
+    rpio.i2cSetSlaveAddress(0x3c);
+    oled.writeString(64, 30, font, displayValue2 + '%  ', 'WHITE', false);
+    oled.update();
   }, 100);
 }
 
