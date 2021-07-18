5603191e19cccf8201c30259814dd5a8743c24f8
Andre <a.michelberger@web.de>
2021-07-18 17:57:06 +0200

    set channel function

--- modules/display.js
+++ modules/display.js
@@ -6,11 +6,18 @@ import font from 'oled-font-5x7';
 
 import config from '../config.json';
 
+// eslint-disable-next-line no-buffer-constructor
+const setChannel = new Buffer([0x01]);
+
 const outputData = config.controller.gpio.displays.outputData;
 
 const outputMultiplexer = config.controller.gpio.displays.outputMultiplexer;
 
 let oled;
+
+function setMultiplexer(params) {
+  
+}
 
 export function initOLED() {
   oled = new Oled({ rpio, address: 0x3c });
@@ -31,6 +38,9 @@ export function initOLED() {
 }
 
 export function setOLED(data) {
+  rpio.i2cSetSlaveAddress(0x70);
+  rpio.i2cWrite(setChannel);
+
   rpio.i2cSetSlaveAddress(0x3c);
   // oled.drawRect(0, 0, 128, 64, 'WHITE');
   oled.writeString(64, 30, font, `${Math.ceil(data[0][0].fader.value * 100)}%  `, 'WHITE', false);
