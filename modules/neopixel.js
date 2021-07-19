f438f572abeb57d23c15c3344b008338e78dd7d0
Andre <a.michelberger@web.de>
2021-07-19 17:41:38 +0200

    neopixel function changed

--- modules/neopixel.js
+++ modules/neopixel.js
@@ -13,40 +13,43 @@ function hexToRgb(hex) {
   } : null;
 }
 
+function rgbColor(red, green, blue, intensity) {
+  if (typeof intensity === 'undefined') { intensity = 1; }
+  // eslint-disable-next-line no-bitwise
+  return ((red * intensity) << 16) | ((green * intensity) << 8) | (blue * intensity);
+}
+
 export function setPixels(data) {
   data.forEach((row, rowMultipier) => {
     row.forEach((button, i) => {
-      let color = {};
-      if (allConfig.maweb.appType === 'dot2') {
-        color.r = config.dot2Color.red;
-        color.g = config.dot2Color.green;
-        color.b = config.dot2Color.blue;
-      } else color = hexToRgb(button.color || button.fader.color);
-      const multipier = config.isRunMultipier;
-      // eslint-disable-next-line no-bitwise
-      let setColor = (color.r * multipier << 16) | (color.g * multipier << 8) | color.b * multipier;
-      if (!button.fader) { // button LEDs
-        // eslint-disable-next-line no-bitwise
-        if (button.isRun) setColor = (color.r << 16) | (color.g << 8) | color.b;
-        // TODO: Better bitwise handler
-        if (button.empty) setColor = 0;
-      } else { // fader LEDs
-        // eslint-disable-next-line no-bitwise
-        if (button.fader.isRun) setColor = (color.r << 16) | (color.g << 8) | color.b;
-        // if (button.fader.isRun) setColor = (color.r * button.fader.value << 16) | (color.g * button.fader.value << 8) | color.b * button.fader.value;
-        // TODO: Better bitwise handler
-        if (button.fader.empty) setColor = 0;
+      // set button basecolor depending on appType:
+      let buttonBaseColor = {};
+      if (allConfig.maweb.appType === 'dot2') { // default color for dot2 from config file
+        buttonBaseColor.red = config.dot2Color.red;
+        buttonBaseColor.green = config.dot2Color.green;
+        buttonBaseColor.blue = config.dot2Color.blue;
+      } else buttonBaseColor = hexToRgb(button.color || button.fader.color);
+      // set led color depending on button state:
+      let ledColor = rgbColor(buttonBaseColor.red, buttonBaseColor.green, buttonBaseColor.blue, config.intensityButtonOff);
+      if (!button.fader) { // button-LEDs
+        // if (button.isRun) ledColorValue = (buttonColor.red << 16) | (buttonColor.green << 8) | buttonColor.blue;
+        if (button.isRun) ledColor = rgbColor(buttonBaseColor.red, buttonBaseColor.green, buttonBaseColor.blue, config.intensityButtonOn);
+        if (button.empty) ledColor = rgbColor(0, 0, 0);
+      } else { // fader-LEDs
+        // if (button.fader.isRun) ledColorValue = (buttonColor.red << 16) | (buttonColor.green << 8) | buttonColor.blue;
+        if (button.fader.isRun) ledColor = rgbColor(buttonBaseColor.red, buttonBaseColor.green, buttonBaseColor.blue, config.intensityButtonOn);
+        if (button.fader.empty) ledColor = rgbColor(0, 0, 0);
       }
-      pixels[i + (rowMultipier * 8)] = setColor;
+      pixels[i + (rowMultipier * 8)] = ledColor;
     });
   });
 }
 
-export function initPixel() {
-  // initalize config
+export function initNeopixels() {
+  // initalize config:
   ledHandler.configure(config.options);
-  // reset pixels
+  // reset pixels:
   pixels.forEach((plx, i) => pixels[i] = 0);
-  // set interval
+  // set update interval:
   setInterval(() => ledHandler.render(pixels), config.options.refreshTime);
 }
