180f016bbcb90e73a643a5d96abc14e4d52ad39d
Andre <a.michelberger@web.de>
2021-07-19 17:55:51 +0200

    optimized neopixel functions

--- modules/neopixel.js
+++ modules/neopixel.js
@@ -14,33 +14,33 @@ function hexToRgb(hex) {
 }
 
 function rgbColor(red, green, blue, intensity) {
-  if (typeof intensity === 'undefined') { intensity = 1; }
+  if (typeof intensity === 'undefined') { intensity = 1; } // default value for intensity
   // eslint-disable-next-line no-bitwise
   return ((red * intensity) << 16) | ((green * intensity) << 8) | (blue * intensity);
 }
 
-export function setPixels(data) {
-  data.forEach((row, rowMultipier) => {
-    row.forEach((button, i) => {
-      // set button basecolor depending on appType:
-      let buttonBaseColor = {};
-      if (allConfig.maweb.appType === 'dot2') { // default color for dot2 from config file
-        buttonBaseColor.red = config.dot2Color.red;
-        buttonBaseColor.green = config.dot2Color.green;
-        buttonBaseColor.blue = config.dot2Color.blue;
-      } else buttonBaseColor = hexToRgb(button.color || button.fader.color);
-      // set led color depending on button state:
+export function setPixels(playbackData) {
+  // set button basecolor depending on appType:
+  let buttonBaseColor = {};
+  if (allConfig.maweb.appType === 'dot2') { // default color for dot2 from config file
+    buttonBaseColor.red = config.dot2Color.red;
+    buttonBaseColor.green = config.dot2Color.green;
+    buttonBaseColor.blue = config.dot2Color.blue;
+  } else buttonBaseColor = hexToRgb(button.color || button.fader.color);
+  // go through all LEDs:
+  playbackData.forEach((row, rowNumber) => {
+    row.forEach((button, columnNumber) => {
+      // set LED color depending on executor-state:
       let ledColor = rgbColor(buttonBaseColor.red, buttonBaseColor.green, buttonBaseColor.blue, config.intensityButtonOff);
       if (!button.fader) { // button-LEDs
-        // if (button.isRun) ledColorValue = (buttonColor.red << 16) | (buttonColor.green << 8) | buttonColor.blue;
         if (button.isRun) ledColor = rgbColor(buttonBaseColor.red, buttonBaseColor.green, buttonBaseColor.blue, config.intensityButtonOn);
         if (button.empty) ledColor = rgbColor(0, 0, 0);
       } else { // fader-LEDs
-        // if (button.fader.isRun) ledColorValue = (buttonColor.red << 16) | (buttonColor.green << 8) | buttonColor.blue;
         if (button.fader.isRun) ledColor = rgbColor(buttonBaseColor.red, buttonBaseColor.green, buttonBaseColor.blue, config.intensityButtonOn);
         if (button.fader.empty) ledColor = rgbColor(0, 0, 0);
       }
-      pixels[i + (rowMultipier * 8)] = ledColor;
+      // set pixel:
+      pixels[columnNumber + (rowNumber * 8)] = ledColor;
     });
   });
 }
