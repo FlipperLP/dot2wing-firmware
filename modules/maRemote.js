fa69b94fd81ee208d601999ce4797b7f12b5542d
Andre <a.michelberger@web.de>
2021-07-19 18:42:40 +0200

    grma debugging

--- modules/maRemote.js
+++ modules/maRemote.js
@@ -59,10 +59,10 @@ export function getPlayback() {
     sendWebsocket({
       requestType: 'playbacks',
       session,
-      startIndex: [0, 100, 200],
-      itemsCount: [8, 8, 8],
+      startIndex: [0, 100],
+      itemsCount: [8, 8],
       pageIndex: 0,
-      itemsType: [2, 3, 3],
+      itemsType: [2, 3],
     });
   }
 }
