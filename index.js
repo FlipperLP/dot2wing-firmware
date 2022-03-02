81a4f113d6984cd312dcb2c494ff247403e524ae
Phil <philip.schaefer0@gmail.com>
2022-03-02 18:20:01 +0100

    [revert] test

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ import { loginSession, websocketAnswer } from './modules/maRemote';
 
 import { initNeopixels } from './modules/neopixel';
 
-import { initEncoder, readEncoder, readEncoderSwitch } from './modules/encoder';
+// import { initEncoder, readEncoder, readEncoderSwitch } from './modules/encoder';
 
-initEncoder();
+// initEncoder();
 
-setInterval(() => {
-  const test = readEncoder();
-  if (test) console.log(test);
-  const test2 = readEncoderSwitch();
-  if (test2 !== null) console.log(test2);
-}, 1);
+// setInterval(() => {
+//   const test = readEncoder();
+//   if (test) console.log(test);
+//   const test2 = readEncoderSwitch();
+//   if (test2 !== null) console.log(test2);
+// }, 1);
 
 // open websocket
-// WSconnection.onopen = () => loginSession();
+WSconnection.onopen = () => loginSession();
 
-// // init neopixel
-// initNeopixels();
+// init neopixel
+initNeopixels();
 
-// // websocket emitter
-// WSconnection.onmessage = (msg) => websocketAnswer(msg);
-// WSconnection.onerror = (error) => console.log(`WebSocket error: ${error}`);
-// WSconnection.onclose = () => {
-//   console.error('Disconnected! Exiting...');
-//   process.exit(1);
-// };
+// websocket emitter
+WSconnection.onmessage = (msg) => websocketAnswer(msg);
+WSconnection.onerror = (error) => console.log(`WebSocket error: ${error}`);
+WSconnection.onclose = () => {
+  console.error('Disconnected! Exiting...');
+  process.exit(1);
+};
