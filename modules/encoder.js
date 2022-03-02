4a9910f945d370f257353a523cbddfa3d27e7f42
Phil <philip.schaefer0@gmail.com>
2022-03-02 17:32:45 +0100

    [fix]

--- modules/encoder.js
+++ modules/encoder.js
@@ -9,10 +9,7 @@ import config from '../config.json';
 export function initEncoder() {
   rpio.init({ gpiomem: false });
   // define gpio pins
-  config.controller.gpio.encoder.pins.forEach((pin) => {
-    console.log(pin);
-    // rpio.open(pin, rpio.INPUT, rpio.PULL_UP);
-  });
+  Object.values(config.controller.gpio.encoder.pins).forEach((pin) => rpio.open(pin, rpio.INPUT, rpio.PULL_UP));
 }
 
 export { initEncoder as default };
